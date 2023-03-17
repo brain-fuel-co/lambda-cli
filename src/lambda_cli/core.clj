bce9aeed81f269bfb1c9e04a5e5267bc14406c82
brain-fuel <matt@brain-fuel.co>
2023-03-17 10:02:07 -0400

    add enlive dependency and fix lambda syntax

--- src/lambda_cli/core.clj
+++ src/lambda_cli/core.clj
@@ -1,6 +1,8 @@
 (ns lambda-cli.core
-  (:require [instaparse.core :as insta]
-            [clojure.pprint :as pp])
+  (:require [clojure.pprint :as pp]
+            [net.cgrand.enlive-html :as enlive]
+            [instaparse.core :as insta])
+
   (:gen-class))
 
 (insta/set-default-output-format! :enlive)
@@ -13,21 +15,33 @@
 (def lambda-calculus
   (insta/parser
    "L_EXP    = VAR_EXP
-             | LAMBDA VAR_EXP '.' L_EXP
-             | '(' L_EXP ')'
-             | L_EXP L_EXP
+             | LAMBDA VAR '.' L_EXP
+             | <'('> L_EXP <')'> <'('> L_EXP <')'>
     VAR_EXP  = VAR
              | NUMBER
              | NUMBER VAR
              | VAR_EXP ARITH_OP VAR_EXP
-    VAR      = #'[a-zA-Z]'
-    NUMBER   = #'[0-9]*'
-    ARITH_OP = '+' | '-' | '*' | '/'
+    VAR      = #'[a-z]' | #'[A-Z]+'
+    NUMBER   = #'[0-9]+'
+    ARITH_OP = ADD | SUB | MUL | DIV
+    ADD      = <'+'>
+    SUB      = <'-'>
+    MUL      = <'*'>
+    DIV      = <'/'>
     LAMBDA   = 'lambda'"
    :auto-whitespace :standard))
 
+(defn- eliminate-nil-fields [input]
+   (->> input))
+    
+
+
+ 
+
+
 (defn- evaluate [input]
-  (lambda-calculus input))
+  (->> (lambda-calculus input)
+       eliminate-nil-fields))
 
 (defn -main
   "This will eventuall be a Lambda-Calculus REPL"
